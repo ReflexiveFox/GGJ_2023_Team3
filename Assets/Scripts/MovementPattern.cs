f49fa605be7d1c20de2470f04d6054217b772632
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2023-02-04 16:23:18 +0100

    Fixed MovementPattern with chasing mechanic

--- Assets/Scripts/MovementPattern.cs
+++ Assets/Scripts/MovementPattern.cs
@@ -3,42 +3,109 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MovementPattern : MonoBehaviour
+namespace RootBoy
 {
-    NavMeshAgent agent;
-    public Transform[] waypoints;
-    int waypointIndex;
-    Vector3 target;
-
-    // Start is called before the first frame update
-    void Start()
+    public class MovementPattern : MonoBehaviour
     {
-        agent = GetComponent<NavMeshAgent>();
-        UpdateDestination();
-    }
+        private PlayerChecker playerChecker;
+        private NavMeshAgent agent;
+        public Transform[] waypoints;
+        private int waypointIndex;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Vector3.Distance(transform.position, target) < 1)
+        private bool isChasingPlayer;
+        private Vector3 target;
+        private Transform _targetTransform;
+
+        private Transform TargetTransform
         {
-            IterateWaypointIndex();
-            UpdateDestination();
+            get => _targetTransform;
+            set
+            {
+                _targetTransform = value;
+                isChasingPlayer = value is not null;
+                if (!isChasingPlayer)
+                    UpdateDestination(waypoints[waypointIndex].position);
+            }
         }
-    }
 
-    void UpdateDestination()
-    {
-        target = waypoints[waypointIndex].position;
-        agent.SetDestination(target);
-    }
-
-    void IterateWaypointIndex()
-    {
-        waypointIndex++;
-        if (waypointIndex == waypoints.Length)
+        private void Awake()
         {
-            waypointIndex = 0;
+            agent = GetComponent<NavMeshAgent>();
+            playerChecker = GetComponentInChildren<PlayerChecker>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            UpdateDestination(waypoints[waypointIndex].position);
+
+            playerChecker.OnPlayerEntered += EngageTarget;
+            playerChecker.OnPlayerLeft += DisengageTarget;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!isChasingPlayer)
+            {
+                if (Vector3.Distance(transform.position, target) < 1)
+                {
+                    IterateWaypointIndex();
+                    UpdateDestination(waypoints[waypointIndex].position);
+                }
+            }
+            else if (TargetTransform != null)
+            {
+                UpdateDestination(TargetTransform.position);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            playerChecker.OnPlayerEntered -= EngageTarget;
+            playerChecker.OnPlayerLeft -= DisengageTarget;
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                TargetTransform = other.transform;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                TargetTransform = null;
+            }
+        }
+
+        void UpdateDestination(Vector3 targetPosition)
+        {
+            target = targetPosition;
+            agent.SetDestination(target);
+        }
+
+        void IterateWaypointIndex()
+        {
+            waypointIndex++;
+            if (waypointIndex == waypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+        }
+
+        private void EngageTarget(GameObject playerGO)
+        {
+            TargetTransform = playerGO.transform;
+        }
+
+        private void DisengageTarget()
+        {
+            TargetTransform = null;
         }
     }
 }
