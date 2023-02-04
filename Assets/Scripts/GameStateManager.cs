ec862f684064f2ce9e7067d4c66048613c0d25e3
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2023-02-04 08:48:32 +0100

    GameStatemanager is an alive entity
    It actively listens for events of other objects and set some gameStates

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,29 +1,56 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RootBoy
 {
-    public class GameStateManager
+    public class GameStateManager : MonoBehaviour
     {
-        private static GameStateManager _instance;
-        public static GameStateManager Instance
-        {
-            get
-            {
-                if (_instance == null)
-                    _instance = new GameStateManager();
-
-                return _instance;
-            }
-        }
-
-        public GameState CurrentGameState { get; private set; }
-
         public delegate void GameStateChangeHandler(GameState newGameState);
         public event GameStateChangeHandler OnGameStateChanged;
 
-        private GameStateManager()
-        {
+        public static GameStateManager Instance;
 
+        public GameState CurrentGameState { get; private set; }
+
+
+        private void Awake()
+        {
+            // if the istance exists and it's not us
+            if (Instance != null && Instance != this)
+            {
+                //destroy this instance 
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+                // don't destroy the game manager while changing the levels
+                DontDestroyOnLoad(gameObject);
+                SceneManager.sceneLoaded += SetGameState;
+                PlayerHealth.OnPlayerDead += SetLostGameState;
+            }
+        }
+
+        private void SetLostGameState()
+        {
+            SetState(GameState.Lost);
+        }
+
+        private void SetGameState(Scene scene, LoadSceneMode loadSceneMode)
+        {
+            if(scene.buildIndex == 0)   //Main menu
+            {
+                SetState(GameState.MainMenu);
+            }
+            else if(scene.buildIndex >= 1)
+            {
+                SetState(GameState.Gameplay);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= SetGameState;
         }
 
         public void SetState(GameState newGameState)
